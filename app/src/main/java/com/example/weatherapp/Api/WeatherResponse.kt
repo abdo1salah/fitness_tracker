8147b42b0580751212480ebf6b8c8f564bb73df9
abdo1salah <100434550+abdo1salah@users.noreply.github.com>
2024-10-03 18:39:13 +0300

    view model

--- app/src/main/java/com/example/weatherapp/Api/WeatherResponse.kt
+++ app/src/main/java/com/example/weatherapp/Api/WeatherResponse.kt
@@ -1,7 +1,7 @@
 package com.example.weatherapp.Api
 
 data class WeatherResponse(
-    //val alerts: Alerts,
+    val alerts: Alerts,
     val current: Current,
     val forecast: Forecast,
     val location: Location
