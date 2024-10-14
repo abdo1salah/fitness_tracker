773a6259fd4ca9e930b80704cadbb9c532d9a07f
Afraim Elkes Eleia <afraimeleia200@gmail.com>
2024-10-14 04:52:07 +0300

    add Location class to get lat and lang

---
+++ app/src/main/java/com/example/weatherapp/location/LocationData.kt
@@ -0,0 +1,66 @@
+package com.example.weatherapp
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import android.util.Log
+import android.widget.Button
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.launch
+
+class LocationData(private val context: Context) {
+    private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+
+    @SuppressLint("MissingPermission")
+    suspend fun getLastLocation(): Flow<Location?> = callbackFlow {
+        if (
+            CheckRequirements.hasPermission(context)
+        ) {
+            if (!CheckRequirements.checkInternetState(context)) {
+                throw Exception("No Internet Connection")
+            } else if (!CheckRequirements.checkGpsState(context)) {
+                throw Exception("Gps is OFF")
+            } else
+                fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                    if (location != null) {
+                        // Do something with the location
+                        Log.d("trace", "LocationData: ${location.latitude}, ${location.longitude}")
+                        launch {
+                            send(location)
+                        }
+
+                    }
+
+                }
+        }
+        awaitClose {
+        }
+    }
+
+}
+
+@Composable
+fun RequestGpsAlertDialog(confirmButton:()->Unit,dismissButton:()->Unit) {
+    AlertDialog(
+        onDismissRequest = {},
+        confirmButton = { TextButton(onClick = { confirmButton() }) { Text("OK") } },
+        dismissButton = { TextButton(onClick = { dismissButton() }) { Text("No, thanks") } },
+        icon = {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_warning_24),
+                contentDescription = "warning"
+            )
+        },
+        text = { Text("For a better experience turn on device location") },
+    )
+}
