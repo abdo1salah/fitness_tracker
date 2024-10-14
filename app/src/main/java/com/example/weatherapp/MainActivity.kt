e5558af2bb2af6007dd9786348ba8ce534a7a189
Afraim Elkes Eleia <afraimeleia200@gmail.com>
2024-10-14 05:14:56 +0300

    Test location with api

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,107 +1,87 @@
 package com.example.weatherapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.weatherapp.Api.WeatherCallable
-import com.example.weatherapp.Api.WeatherResponse
-import com.example.weatherapp.Api.getEndPoint
+import com.example.weatherapp.location.CheckRequirements
+import com.example.weatherapp.location.LocationPermissionScreen
+import com.example.weatherapp.location.PermissionDeniedDialog
+import com.example.weatherapp.location.RequestGpsAlertDialog
 import com.example.weatherapp.ui.theme.WeatherAppTheme
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+
 
 class MainActivity : ComponentActivity() {
+    lateinit var weatherViewModel: WeatherViewModel
+    var isDialogGpsShown: Boolean by mutableStateOf(true)
+    var isDialogShown: Boolean by mutableStateOf(false)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            weatherViewModel = viewModel()
             WeatherAppTheme {
-                //getEndPoint("61.5240","105.3188")
-                val weatherViewModel : WeatherViewModel = viewModel()
-                val temp = weatherViewModel.casheddata?.current?.temp_c.toString()
-                val tempF = weatherViewModel.casheddata?.current?.temp_f.toString()
-                val p = weatherViewModel.casheddata?.current?.is_day.toString()
-                val p2 = weatherViewModel.casheddata?.current?.feelslike_c.toString()
-               val f= weatherViewModel.casheddata?.forecast?.forecastday?.get(0)?.day?.avgtemp_c.toString()
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column {
-                    Greeting(
-                        name =temp,
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    Greeting(
-                        name =tempF,
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    Greeting(
-                        name =p,
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    Greeting(
-                        name =p2,
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Log.d("trac", "recomposition")
+                if (isDialogShown) {
+                    PermissionDeniedDialog { isDialogShown = false }
                 }
+                if (!weatherViewModel.hasPermission) {
+                    LocationPermissionScreen(
+                        permissionGranted = {
+                            isDialogShown = false
+                            weatherViewModel.refreshData()
+                            weatherViewModel.hasPermission = true
+                        },
+                        permissionDenied = {
+                            isDialogShown = true
+                        },
+                    )
+                } else {
+                    if (!weatherViewModel.hasInternetConnection) {
+                        Toast.makeText(this, "No Internet Connection", Toast.LENGTH_SHORT).show()
+
+                    } else if (!weatherViewModel.hasGps) {
+                        if (isDialogGpsShown)
+                            RequestGpsAlertDialog(confirmButton = {
+                                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                                startActivity(intent)
+                                isDialogGpsShown = false
+                            }, dismissButton = {
+                                isDialogGpsShown = false
+                            })
+                    }
+                    Log.d("trace", "response ${weatherViewModel.casheddata.toString()}")
+                    if (weatherViewModel.casheddata == null) {
+                        CircularProgressIndicator(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .wrapContentSize(Alignment.Center)
+                        )
+                    } else
+                        HomeScreen(weatherResponse = weatherViewModel.casheddata, hasGps = weatherViewModel.hasGps)
                 }
             }
+
         }
     }
-
-
-    @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("trace", "onRestart")
+        weatherViewModel.hasGps = CheckRequirements.checkGpsState(this)
     }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun GreetingPreview() {
-        WeatherAppTheme {
-            val weatherViewModel: WeatherViewModel = viewModel()
-            val temp = weatherViewModel.casheddata?.current?.temp_c.toString()
-            val tempF = weatherViewModel.casheddata?.current?.temp_f.toString()
-            val p = weatherViewModel.casheddata?.current?.is_day.toString()
-            val p2 = weatherViewModel.casheddata?.current?.feelslike_c.toString()
-            val f: String = weatherViewModel.casheddata.toString()
-            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                Column {
-                    Greeting(
-                        name = temp,
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    Greeting(
-                        name = tempF,
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    Greeting(
-                        name = p,
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    Greeting(
-                        name = f,
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-            }
-        }
 }
