24e7ac2038db13395228a16659ed547a91347873
Afraim Elkes Eleia <afraimeleia200@gmail.com>
2024-10-15 21:40:57 +0300

    Add Permission Screen

--- app/src/main/java/com/example/weatherapp/util/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherapp/util/WeatherViewModel.kt
@@ -1,55 +1,51 @@
 package com.example.weatherapp.util
 
-import android.app.Activity
 import android.app.Application
-import android.content.Context
-import android.content.ContextWrapper
 import android.util.Log
-import androidx.lifecycle.viewModelScope
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
-import com.example.weatherapp.data.model.WeatherResponse
+import androidx.lifecycle.viewModelScope
 import com.example.weatherapp.data.SearchApi.SEARCHENDPOINT
 import com.example.weatherapp.data.SearchApi.SearchApi
 import com.example.weatherapp.data.SearchApi.SearchItem
 import com.example.weatherapp.data.SearchApi.getSearchEndPoint
+import com.example.weatherapp.data.model.WeatherResponse
 import com.example.weatherapp.data.repository.WeatherRepo
+import com.example.weatherapp.location.CheckRequirements
 import kotlinx.coroutines.launch
 
 class WeatherViewModel(app: Application) : AndroidViewModel(app) {
 
     private val repo = WeatherRepo(app)
     var casheddata: WeatherResponse? by mutableStateOf(null)
-    var searchData : List<SearchItem> by mutableStateOf(emptyList())
-
+    var searchData: List<SearchItem> by mutableStateOf(emptyList())
+    var hasPermission: Boolean by mutableStateOf(true)
 
     var selectedWindSpeedUnit by mutableStateOf("")
     var selectedTempUnit by mutableStateOf("")
 
 
-    private fun refreshData(){
+     fun refreshData() {
         viewModelScope.launch {
             try {
                 repo.refreshData()
-            }
-            catch (e :Exception){
-                Log.d("trace",e.message.toString())
+            } catch (e: Exception) {
+                Log.d("trace", e.message.toString())
             }
 
             casheddata = repo.getCashedData()
         }
     }
 
-    fun updateSearchData(search:String){
+    fun updateSearchData(search: String) {
         getSearchEndPoint(search)
         viewModelScope.launch {
             try {
                 searchData = SearchApi.searchService.getSearchData(SEARCHENDPOINT)
-            }
-            catch (e :Exception){
-                Log.d("trace",e.message.toString())
+            } catch (e: Exception) {
+                Log.d("trace", e.message.toString())
             }
 
         }
@@ -66,10 +62,12 @@ class WeatherViewModel(app: Application) : AndroidViewModel(app) {
             }
         }
     }
+
     init {
         viewModelScope.launch {
             casheddata = repo.getCashedData()
         }
+        hasPermission = CheckRequirements.hasPermission(app)
         refreshData()
     }
 
